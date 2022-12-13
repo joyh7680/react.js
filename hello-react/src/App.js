d14d5e1af199422b198cc8d00863beafa423f3ee
joyh7680 <76725322+joyh7680@users.noreply.github.com>
2022-12-13 16:25:48 +0900

    Add files via upload
    1 주차 react code update.
    original folder는 react_jiwon에 있음

---
+++ hello-react/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const name = '리액트';
+    return <div className="react">{name}</div>
+  }
+}
+
+export default App;
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MyComponent = () => {
+    return <div>나의 첫 컴포넌트</div>;
+
+};
+
+
+export default MyComponent;
+
